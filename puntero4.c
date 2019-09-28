8e2b76b93e19cba913a614459b95536b2fc1f9dc
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2019-09-28 14:53:59 -0300

    Correct by ref parameter passing

---
+++ puntero4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void duplicar( int * a )
+{
+	*a *= 2;
+}
+
+int main()
+{
+	int a = 5;
+
+	printf( "Antes de duplicar a = %d\n", a );
+	duplicar(&a);
+	printf( "Despues de duplicar a = %d\n", a );
+}
