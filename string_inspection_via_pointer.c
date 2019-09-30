d9e44ce241dac995ea2d92fea918f0a6e249c853
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2019-09-30 12:06:17 -0300

    Same example but using pointer notation

---
+++ string_inspection_via_pointer.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	char name[5] = "Mauro";
+
+	printf("Nombre = %s (%p) \n", name, name);
+
+	for ( int i; i < 5; i++ ) {
+		printf( "name[%d](%p) = %c\n", i, name + i, *(name + i) );
+	}
+}
