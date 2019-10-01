48097b41d168b04a9914bdbf47bec97c3a70e7c9
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2019-10-01 08:46:11 -0300

    Example of pointer to struct

---
+++ pointer_to_struct.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct {
+		char name[100];
+		int age;
+	} PERSON;
+
+void fillPersonData( PERSON * person, const char * name, int age )
+{
+	if ( strlen( name ) < 100 ) {
+		strcpy( (*person).name, name );
+	}
+	(*person).age = age;
+}
+
+int main( int argc, const char * argv[] )
+{
+	PERSON person;
+
+	if ( argc < 3 ) {
+		printf( "Indique nombre y edad por favor\n" );
+
+		return 1;
+	}
+
+	fillPersonData( &person, argv[1], atoi( argv[2] ) );	
+	printf( "Name = %s, age = %d\n", person.name, person.age );
+
+	return 0;
+}
