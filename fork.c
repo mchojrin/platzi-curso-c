828207c67d71743b1c7c4ba2d7239e30fe82c3ed
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2019-10-02 20:47:19 -0300

    Improving example

--- fork.c
+++ fork.c
@@ -9,9 +9,9 @@ int main( int argc, const char * argv[] )
 	int pid = fork();
 
 	if ( pid ) {
-		printf( "Luke I am your father. Your PID is %d\n", pid );
+		printf( "Luke I am your father. My PID is %d and your PID is %d\n", getpid(), pid );
 	} else {
-		printf( "I am Luke\n" );
+		printf( "I am Luke. My PID is %d\n", getpid() );
 	}
 
 	while(1);
