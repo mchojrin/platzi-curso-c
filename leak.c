1a1a7e057339eb54111e1a983bf401032673735f
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2019-10-05 16:34:56 -0300

    Memory leak example

---
+++ leak.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main( int argc, char * argv[] )
+{
+	char * p;
+
+	for ( int i = 0; i < 10; i++ ) {
+		printf( "%d - Alocando 50 chars\n", i );
+		p = malloc(sizeof(char) * 50);
+		sleep(1);
+	}
+	printf("Liberando p\n");
+	free(p);
+	printf( "Terminado!\n" );
+}
