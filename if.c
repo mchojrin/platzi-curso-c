54daa51e496b673beae032113c7f24882aef3d20
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2019-10-10 15:30:56 -0300

    Examples of conditional structures

---
+++ if.c
@@ -0,0 +1,10 @@
+int main()
+{
+	int a = 1;
+
+	if ( a > 0 ) {
+		a = 2;
+	} else {
+		a = 3;
+	}
+}
---
+++ switch.c
@@ -0,0 +1,18 @@
+int main()
+{
+	int a = 1;
+
+	switch ( a ) {
+		case 0:
+			a = 2;
+			break;
+		case 1:
+			a = 7;
+			break;
+		case 2:
+			a = 6;
+			break;
+		default:
+			a = 10;
+	}
+}
