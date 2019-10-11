709d2c8aadc079ac6b1fe21102164d498d67fc58
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2019-10-11 11:02:45 -0300

    Renaming for clarity

--- pointer_assig.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	int number;
-	int * pointToNumber = &number;
-
-	printf("%p", pointToNumber);
-}
---
+++ print_var_address.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+	int number;
+
+	printf("%p", &pointToNumber);
+}
