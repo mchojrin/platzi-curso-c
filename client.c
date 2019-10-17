cda6d3b9459c71cfc5347779c0bbe8fbc46312bd
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2019-10-17 10:28:34 -0500

    Improved sockets examples

--- client.c
+++ client.c
@@ -1,42 +1,65 @@
-// Client side C/C++ program to demonstrate Socket programming 
-#include <stdio.h> 
-#include <sys/socket.h> 
-#include <arpa/inet.h> 
-#include <unistd.h> 
-#include <string.h> 
-#define PORT 8080 
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char const *argv[]) 
-{ 
-	int sock = 0, valread; 
-	struct sockaddr_in serv_addr; 
-	char *hello = "Hello from client"; 
-	char buffer[1024] = {0}; 
-	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
-	{ 
-		printf("\n Socket creation error \n"); 
-		return -1; 
-	} 
+int main(int argc, char *argv[])
+{
+	if(argc > 2)
+	{
+		//Primer paso, definir variables
+		char *ip;
+		int client_socket, numbytes,puerto;
+		char buf[100];
+		puerto=atoi(argv[2]);
+		ip=argv[1];
 
-	serv_addr.sin_family = AF_INET; 
-	serv_addr.sin_port = htons(PORT); 
-	
-	// Convert IPv4 and IPv6 addresses from text to binary form 
-	if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
-	{ 
-		printf("\nInvalid address/ Address not supported \n"); 
-		return -1; 
-	} 
+		struct sockaddr_in server;
+		/* informaciÃ³n sobre la direcciÃ³n del servidor */
 
-	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
-	{ 
-		printf("\nConnection Failed \n"); 
-		return -1; 
-	} 
-	send(sock , hello , strlen(hello) , 0 ); 
-	printf("Hello message sent\n"); 
-	valread = read( sock , buffer, 1024); 
-	printf("%s\n",buffer ); 
-	return 0; 
-} 
+		if(inet_pton(AF_INET, argv[1], &server.sin_addr)<=0) { 
+			printf("\nInvalid address/ Address not supported \n"); 
+			return -1; 
+		} 
 
+		printf( "Abriendo un socket\n" );
+		//Paso 2, definicion de socket
+		if ((client_socket=socket(AF_INET, SOCK_STREAM, 0))==-1){
+			/* llamada a socket() */
+			printf("socket() error\n");
+			exit(-1);
+		}
+		
+		//Datos del servidor
+		server.sin_family = AF_INET;
+		server.sin_port = htons(puerto);
+		bzero(&(server.sin_zero),8);
+
+		printf( "Conectando a %s:%s\n", argv[1], argv[2] );
+		//Paso 3, conectarnos al servidor
+		if(connect(client_socket, (struct sockaddr *)&server, sizeof(struct sockaddr))==-1){
+			/* llamada a connect() */
+			printf("connect() error\n");
+			exit(-1);
+		}
+
+		printf( "Recibiendo...\n" );
+		if ((numbytes=recv(client_socket,buf,100,0)) == -1){
+			/* llamada a recv() */
+			printf("Error en recv() \n");
+			exit(-1);
+		}
+
+		buf[numbytes]='\0';
+
+		printf("Mensaje del Servidor: %s\n",buf);
+		 /* muestra el mensaje de bienvenida del servidor =) */
+
+		shutdown(client_socket, 2);
+	} else{
+		printf("No se ingreso el ip y puerto por parametro\n");
+	}
+}
--- server.c
+++ server.c
@@ -1,54 +1,73 @@
-// Server side C/C++ program to demonstrate Socket programming 
-#include <unistd.h> 
-#include <stdio.h> 
-#include <sys/socket.h> 
-#include <stdlib.h> 
-#include <netinet/in.h> 
-#include <string.h> 
-#define PORT 8080 
+// Ficheros de cabecera
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
 
-int main(int argc, char const *argv[]) 
-{ 
-	int server_fd, new_socket, valread; 
-	struct sockaddr_in address; 
-	int opt = 1; 
-	int addrlen = sizeof(address); 
-	char buffer[1024] = {0}; 
-	char *hello = "Hello from server"; 
-	
-	// Creating socket file descriptor 
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) { 
-		perror("socket failed"); 
-		exit(EXIT_FAILURE); 
-	} 
-	
-	// Forcefully attaching socket to the port 8080 
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) { 
-		perror("setsockopt"); 
-		exit(EXIT_FAILURE); 
-	} 
-	address.sin_family = AF_INET; 
-	address.sin_addr.s_addr = INADDR_ANY; 
-	address.sin_port = htons( PORT ); 
-	
-	// Forcefully attaching socket to the port 8080 
-	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) { 
-		perror("bind failed"); 
-		exit(EXIT_FAILURE); 
-	} 
-	if (listen(server_fd, 3) < 0) { 
-		perror("listen"); 
-		exit(EXIT_FAILURE); 
-	} 
-	if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) { 
-		perror("accept"); 
-		exit(EXIT_FAILURE); 
-	} 
-	valread = read( new_socket , buffer, 1024); 
-	printf("%s\n",buffer ); 
-	send(new_socket , hello , strlen(hello) , 0 ); 
-	printf("Hello message sent\n"); 
+int main(int argc, char **argv)
+{
+	if(argc > 1)
+	{
+		//Primer paso, definir variables
+		int server_socket,client_socket,longitud_cliente,puerto;
+		
+		puerto = atoi(argv[1]);
 
-	return 0; 
-} 
+		//Se necesitan dos estructuras del tipo sockaddr
+		//La primera guarda info del server
+		//La segunda del cliente
+		struct sockaddr_in server;
+		struct sockaddr_in client;
 
+		//Configuracion del servidor
+		server.sin_family= AF_INET; //Familia TCP/IP
+		server.sin_port = htons(puerto); //Puerto
+		server.sin_addr.s_addr = INADDR_ANY; //Cualquier cliente puede conectarse
+		bzero(&(server.sin_zero),8); //Funcion que rellena con 0's
+
+		//Paso 2, definicion de socket
+		if (( server_socket=socket(AF_INET,SOCK_STREAM,0) )<0){
+			perror("Error de apertura de socket");
+			exit(-1);
+		}
+
+		//Paso 3, avisar al sistema que se creo un socket
+		if( bind( server_socket,(struct sockaddr*)&server, sizeof(struct sockaddr))==-1) {
+			printf("error en bind() \n");
+			exit(-1);
+		}
+
+		//Paso 4, establecer el socket en modo escucha
+		if ( listen(server_socket,5) == -1 ) {
+			printf("error en listen()\n");
+			exit(-1);
+		}
+
+		//Paso5, aceptar conexiones
+		longitud_cliente= sizeof(struct sockaddr_in);
+		printf("Todo listo! Esperando clientes en el puerto %d\n", puerto);
+		/* A continuaciÃ³n la llamada a accept() */
+		if ( (client_socket = accept(server_socket,(struct sockaddr *)&client,&longitud_cliente))==-1 ) {
+			printf("error en accept()\n");
+			exit(-1);
+		}
+		
+		char str[INET_ADDRSTRLEN];
+		inet_ntop(AF_INET, &(client.sin_addr), str, INET_ADDRSTRLEN);
+		printf( "Se conecto un cliente desde %s:%d. Lo saludo\n", str, client.sin_port );
+
+		send(client_socket,"Bienvenido a mi servidor.\n",26,0);
+		
+		printf( "Saludo enviado!\n" );
+		shutdown( client_socket, 2 );
+		shutdown( server_socket, 2 );
+		
+	} else {
+		printf("NO se ingreso el puerto por parametro\n");
+	}
+
+	return 0;
+}
